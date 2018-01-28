381ee1d3495b68c23fbf4a2763f3512c9ec44f77
Neamar <neamar@neamar.fr>
2018-01-28 15:09:48 -0800

    Generate epitrochoid from SVG

--- scripts/generate-epitrochoid-for-svg.js
+++ scripts/generate-epitrochoid-for-svg.js
@@ -1,7 +1,15 @@
 "use strict";
 
-let R = 126;
-let r = R / 5;
+// Number of spires
+let n = 5;
+
+// Main gear radius
+let R = 128;
+
+// Secondary gear radius
+let r = R / n;
+
+// Pen radius
 let d = 60;
 
 let cX = 250;
@@ -9,11 +17,12 @@ let cY = 250;
 
 let points = [];
 
-for(var i = 0; i < 100; i += 1) {
-  let theta = i / 100 * 2 * Math.PI;
-
+const NB_POINTS = 200;
+for(var i = 0; i < NB_POINTS; i += 1) {
+  let theta = i / NB_POINTS * 2 * Math.PI;
+  let theta2 = theta - 2 * Math.PI / n;
   points.push(
-      (cX + (R + r) * Math.cos(theta) - d * Math.cos((R + r) / r * theta)) + " " + (cY + (R + r) * Math.sin(theta) - d * Math.sin((R + r) / r * theta))
+      (cX + (R + r) * Math.cos(theta) - d * Math.cos((R + r) / r * theta2)) + " " + (cY + (R + r) * Math.sin(theta) - d * Math.sin((R + r) / r * theta2))
   );
 }
 
@@ -26,3 +35,4 @@ let svg = '<svg width="500" height="500" xmlns="http://www.w3.org/2000/svg">' + 
 svg += path;
 svg += "\n</svg>";
 require('fs').writeFileSync('/tmp/test.svg', svg);
+require('fs').writeFileSync('/tmp/path.svg', path);
